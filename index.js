882e1e4c396d81d5c3f55de8a4754f9823cc37cc
richardtam.ny <richardtam.ny@gmail.com>
2019-02-04 20:22:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return "Welcome, " + name + "." + " You are number " + katzDeliLine.length + " in line."
+}
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  } else {
+
+    return "Currently serving " + katzDeliLine.shift() + "."
+  }
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  } else if (katzDeliLine.length === 1) {
+    return "Currently serving" + katzDeliLine[0] +" ."
+  } else {
+    var x = []
+    for(var i = 0; i < katzDeliLine.length; i++) {
+      x.push(i+1 + ". " + katzDeliLine[i])
+    }
+    return "The line is currently: " + x.join(", ")
+  }
+  }
